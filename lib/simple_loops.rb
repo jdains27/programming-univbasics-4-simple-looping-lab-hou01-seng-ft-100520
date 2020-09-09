d07788d81b7b403b9991b43364c3a9c0be0166e8
Jordan Dains <dains.jordan2@gmail.com>
2020-09-09 17:13:17 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,16 +1,9 @@
-def loop_message_five_times(string)
-  count = 0 
-  while count < (5)
-  count +=1
-  end
-end
-
-def loop_message_n_times(pokemon)
-  count = 0 
+def loop_message_five_times(pokemon)
+  count = 0
   while count < 5 do
     puts pokemon
-    count += 1 
-  end  
+    count += 1
+  end
 end
 
 def output_array
